0f4f373ea20ddfcecc773678940d9ea669f4fa0e
Glitch (spurious-relish) <none>
2018-01-15 22:55:54 +0000

    🌺📕 Checkpoint server.js:533956/1

--- server.js
+++ server.js
@@ -26,7 +26,6 @@ app.get("/scoreboard", function (req, res) {
   url = 'https://api.mysportsfeeds.com/v1.1/pull/nfl/' + season_name + '/scoreboard.' + format + '?fordate=' + for_date,
   auth = 'Basic ' + new Buffer(username+ ':' + password).toString('base64');
   
-
   request(
       {
         url : url,
@@ -37,7 +36,6 @@ app.get("/scoreboard", function (req, res) {
       function (error, response, body) {
         console.log('error:', error); // Print the error if one occurred
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        //res.send(typeof body);
         res.json(JSON.parse(body));
       }
   );
