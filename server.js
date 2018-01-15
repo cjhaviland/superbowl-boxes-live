9cc32f7fafc992d2b7faf1de92d6f61b14981fa5
Glitch (spurious-relish) <none>
2018-01-15 19:53:47 +0000

    Exporting to cjhaviland/superbowl-boxes-live

--- server.js
+++ server.js
@@ -4,15 +4,7 @@
 // init project
 let express = require('express'),
   app = express(),
-  request = require('request'),
-  bodyParser = require('body-parser');
-
-//// body parser config
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }))
-
-// parse application/json
-app.use(bodyParser.json())
+  request = require('request');
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -46,7 +38,7 @@ app.get("/scoreboard", function (req, res) {
         console.log('error:', error); // Print the error if one occurred
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
         //res.send(typeof body);
-        res.send(body);
+        res.json(JSON.parse(body));
       }
   );
 });
