2f86b7cfa1a95638fe39f32ed8fbba7f048010ac
Glitch (spurious-relish) <none>
2018-01-19 03:02:15 +0000

    🎩🛫 Checkpoint public/client.js:533956/227 public/style.css:533956/117 views/index.html:533956/144

--- public/client.js
+++ public/client.js
@@ -3,23 +3,15 @@
 
 // by default, you've got jQuery,
 // add other scripts at the bottom of index.html
-let hundredGrids = '';
+let tableEl = document.querySelector('table');
 
 
 $(function() {
-  createGrids();
-
-  $(hundredGrids).insertAfter('.nfc');
+  console.log(tableEl);
 });
-
-let createGrids = () => {
-  for (var i = 0; i < 100; i++){
-    hundredGrids += `<div id="box-${i}"></div>`;
-  }
-}
 
 // function to crawl through the the table that uses a callback function
 
 let scanTable = (callback) => {
-    
+  
 }
