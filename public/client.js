c369532bab63d753a9ac76c4a79937dfb647b7ae
Glitch (spurious-relish) <none>
2018-01-24 18:27:40 +0000

    Fixed Eagles CSS and added Winner table

--- public/client.js
+++ public/client.js
@@ -1,6 +1,13 @@
 // client-side js
 // run by the browser each time your view template is loaded
 
+// Stuff to add
+/*
+ * Probability Styling: http://media1.break.com/dnet/media/2013/2/1/c77fc888-97a3-433d-85da-c8c088a43204.jpg
+ * Winner styling/grid
+*/
+
+
 //let numArray = [0,1,2,3,4,5,6,7,8,9];
 let scoreObj = {};
 //let awayArray = [9,8,7,4,3,2,1,0,5,6]
@@ -28,19 +35,20 @@ $(function() {
   fillNumCells(topRow, awayArray);
   fillNumCells(sideColumn, homeArray);
   
-  // For testing
-  //title.on('click', () => {
-     
-  //}); // End title on click
+  // Every 5 mins refresh API data
+  timeout = setTimeout(function(){
+    scoreboardApi();
+  }, 300000);
   
-  // Start API refresh
-  scoreboardApi();   
+  // Run once
+  scoreboardApi();
   
-  // On Hoverfor highlighting
+  // On Hover for highlighting
  $('.superbowl-pool-grid td:not(td[id$="-0"]):gt(9)').hover(function(){
-   $(`td:contains(${ $(this).html() })`).toggleClass('winner');
- });
-}); // on ready function
+   $(`td:contains(${ $(this).html() })`).toggleClass('highlight');
+ }); // end hover listener 
+  
+}); // end on ready function
 
 
 // Add participants to grid
@@ -63,20 +71,16 @@ let scoreboardApi = () => {
   })
   .done(() => {
     updateScores();
-    updateWinners();
+    
+    // If the game is over kill the refresh
+    if (scoreObj.isCompleted == 'true' || scoreObj.isInProgress == 'false'){
+      clearTimeout(timeout);
+      console.log('Cleared');
+    }
+    else {
+      console.log('Refreshed');
+    }
   });
-  
-  console.log('Refreshed');
-  
-  // Every 10 mins refresh API data
-  timeout = setTimeout(function(){
-    scoreboardApi();
-  }, 600000);
-  
-  // If the game is over kill the refresh
-  if (scoreObj.isCompleted == 'true'){
-    clearTimeout(timeout);
-  }
 } // end scoreboardApi
 
 // Updates the scorebox
@@ -84,12 +88,18 @@ let updateScores = () => {
   let awayScore = 0;
   let homeScore = 0;
   
+  // Fill Abbreviations into Scoreboard
   awayScoreboard[0].innerHTML = scoreObj.game.awayTeam.Abbreviation;
   homeScoreboard[0].innerHTML = scoreObj.game.homeTeam.Abbreviation;
+  
+  // Fill Teamnames into grid sidebars
   document.querySelector('.away-team-logo').innerHTML = scoreObj.game.awayTeam.Name;
   document.querySelector('.home-team-logo').innerHTML = scoreObj.game.homeTeam.Name;
+  
+  // Update last updated time
   document.querySelector('#lastUpdated').innerHTML = `Last Updated: ${lastUpdated}`;
   
+  // If the game is in progress or completed
   if (scoreObj.isInProgress != 'false' || scoreObj.isCompleted == 'true') {
     let quarters = scoreObj.quarterSummary.quarter;
     
@@ -106,6 +116,9 @@ let updateScores = () => {
       awayScoreboard[6].innerHTML = awayScore;
       homeScoreboard[6].innerHTML = homeScore;
     }
+    
+    // Update the winners
+    updateWinners();
   }
   
   if (awayScoreboard[5].innerHTML == '' && homeScoreboard[5].innerHTML == ''){
@@ -126,6 +139,7 @@ let updateWinners = () => {
     let awayRow = document.querySelector(`#row-0 [data-scorenum='${parseInt(away) % 10}']`).dataset.num;
     let homeCol = document.querySelector(`td[id$='-0'][data-scorenum='${parseInt(home) % 10}']`).dataset.num;
     
+    // Need to re-write this for the separate quarters
     if (away != '' && home != ''){
       $(`#box-${homeCol}-${awayRow}`).addClass('winner');
       console.log(`#box-${homeCol}-${awayRow} ${parseInt(away) % 10} ${parseInt(home) % 10}`);
