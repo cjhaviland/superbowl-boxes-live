b44d975a1160d8dfb298948bd7b71e3857d8276d
Glitch (spurious-relish) <none>
2018-01-24 02:17:13 +0000

    Better styling and hover effect for finding other grid items

--- public/client.js
+++ public/client.js
@@ -3,8 +3,13 @@
 
 //let numArray = [0,1,2,3,4,5,6,7,8,9];
 let scoreObj = {};
-let awayArray = [9,8,7,4,3,2,1,0,5,6]
-let homeArray = [3,0,2,5,6,4,8,7,1,9]
+//let awayArray = [9,8,7,4,3,2,1,0,5,6]
+//let homeArray = [3,0,2,5,6,4,8,7,1,9]
+let timeout;
+let lastUpdated;
+
+let awayArray = [1,2,3,4,5,6,7,8,9,10];
+let homeArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
 
 // Table Elements
 let tableCells = $('td'),
@@ -28,13 +33,13 @@ $(function() {
      
   //}); // End title on click
   
-  // Run once on load
+  // Start API refresh
   scoreboardApi();   
   
-  // Every 10 mins refresh API data
-  setInterval(() => {
-    scoreboardApi();
-  }, 600000);
+  // On Hoverfor highlighting
+ $('.superbowl-pool-grid td:not(td[id$="-0"]):gt(9)').hover(function(){
+   $(`td:contains(${ $(this).html() })`).toggleClass('winner');
+ });
 }); // on ready function
 
 
@@ -53,12 +58,25 @@ let fillParticipants = () => {
 // Scoreboard API
 let scoreboardApi = () => {
   $.getJSON('https://spurious-relish.glitch.me/scoreboard', function(data){
-    scoreObj = data.scoreboard.gameScore[1];
+    lastUpdated = data.scoreboard.lastUpdatedOn;
+    scoreObj = data.scoreboard.gameScore[0];
   })
   .done(() => {
     updateScores();
     updateWinners();
   });
+  
+  console.log('Refreshed');
+  
+  // Every 10 mins refresh API data
+  timeout = setTimeout(function(){
+    scoreboardApi();
+  }, 600000);
+  
+  // If the game is over kill the refresh
+  if (scoreObj.isCompleted == 'true'){
+    clearTimeout(timeout);
+  }
 } // end scoreboardApi
 
 // Updates the scorebox
@@ -68,6 +86,9 @@ let updateScores = () => {
   
   awayScoreboard[0].innerHTML = scoreObj.game.awayTeam.Abbreviation;
   homeScoreboard[0].innerHTML = scoreObj.game.homeTeam.Abbreviation;
+  document.querySelector('.away-team-logo').innerHTML = scoreObj.game.awayTeam.Name;
+  document.querySelector('.home-team-logo').innerHTML = scoreObj.game.homeTeam.Name;
+  document.querySelector('#lastUpdated').innerHTML = `Last Updated: ${lastUpdated}`;
   
   if (scoreObj.isInProgress != 'false' || scoreObj.isCompleted == 'true') {
     let quarters = scoreObj.quarterSummary.quarter;
@@ -88,7 +109,7 @@ let updateScores = () => {
   }
   
   if (awayScoreboard[5].innerHTML == '' && homeScoreboard[5].innerHTML == ''){
-    $('td:nth-child(6),th:nth-child(6)').hide();
+    $('#scoreboard-grid td:nth-child(6), th:nth-child(6)').hide();
   }
   
 }// end updateScores
